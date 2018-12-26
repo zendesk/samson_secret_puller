e0058643e3c5bc0bc86b5c3a7721dff5aae77c9b
Michael Grosser <michael@grosser.it>
2018-12-26 15:17:18 +0100

    improve docs

---
+++ test/misc_test.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+
+SingleCov.not_covered!
+
+describe ",osc" do
+  it "documents all env vars" do
+    env = /[A-Z_\d]{5,}/
+    supported = File.read("bin/secrets").scan(env) - ["STDOUT", "KUBERNETES_PORT_443_TCP_ADDR", "TESTING"]
+    documented = File.read("README.md").scan(env) - ["README"]
+    (documented - supported).must_equal []
+    (supported - documented).must_equal []
+  end
+end
